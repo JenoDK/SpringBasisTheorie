d50edde8515ce388a8ea6734806b5ba2775b055b
Jeno De Keyzer <jenodekeyzer@gmail.com>
2015-06-18 13:56:32 +0200

    aazaz

---
+++ src/main/java/be/vdab/Main.java
@@ -0,0 +1,20 @@
+package be.vdab;
+
+import java.math.BigDecimal;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import be.vdab.services.EuroService;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"container.xml")) {
+			System.out.println(context.getBean(EuroService.class).naarDollar(
+					BigDecimal.valueOf(2)));
+		}
+
+	}
+
+}
